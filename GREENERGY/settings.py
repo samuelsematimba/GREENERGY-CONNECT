7738d4a9c0dbae0020f1e391c6125400af0bfc2f
samuelsematimba14@gmail.com <samuelsematimba14@gmail.com>
2026-05-06 10:03:00 +0300

    changed a few things on the settings getting ready to change deployment to render

--- GREENERGY/settings.py
+++ GREENERGY/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x+%hml=v!t5@*z70kxk31-r($j=8(z3jw3tmu8yu^idrl(v#6v'
+SECRET_KEY = os.environ.get ('SECRET_KEY', 'django-insecure-x+%hml=v!t5@*z70kxk31-r($j=8(z3jw3tmu8yu^idrl(v#6v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 
 
@@ -81,10 +87,10 @@ WSGI_APPLICATION = 'GREENERGY.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
