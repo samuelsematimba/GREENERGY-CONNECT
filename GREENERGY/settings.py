6f4a2608ce1be8b2c47c5c6f3e3bcf767729d2f8
samuelsematimba14@gmail.com <samuelsematimba14@gmail.com>
2026-05-06 16:03:22 +0300

    fixing heroku error

--- GREENERGY/settings.py
+++ GREENERGY/settings.py
@@ -54,13 +54,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'GREENERGY.urls'
@@ -143,8 +144,7 @@ MEDIA_ROOT= BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #heroku settings.
-import django_heroku
-django_heroku.settings(locals())
+
 
 
 #auth redirects
--- build.sh
+++ build.sh
@@ -4,3 +4,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if not exists
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='SAMUEL').exists():
+    User.objects.create_superuser('SAMUEL', 'samuelsematimba14@gmail.com', 'semat@123')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+"
